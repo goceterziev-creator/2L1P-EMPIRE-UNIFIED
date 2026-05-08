29b2e24416da18742a3d7216257285e44d708ffe
goceterziev-creator <goce_terziev@abv.bg>
2026-05-08 09:54:25 +0300

    Delete 01_ARCHIVE directory

--- 01_ARCHIVE/2l1p-neural-travel-v3-CLEAN-MASTER/modules/date.js
+++
@@ -1,3 +0,0 @@
-function pad(n){return String(n).padStart(2,'0');} 
-function formatDateSafe(dateStr){const d=new Date(dateStr);return pad(d.getDate())+'.'+pad(d.getMonth()+1)+'.'+d.getFullYear()+' '+pad(d.getHours())+':'+pad(d.getMinutes());} 
-module.exports={formatDateSafe}; 
--- 01_ARCHIVE/2l1p-neural-travel-v4/server.js
+++
@@ -1,5 +0,0 @@
-const express = require("express");
-const path = require("path");
-const offersRouter = require("./routes/offers");
-
-const app =
